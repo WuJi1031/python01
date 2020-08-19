b697c8874466c6cb788ecdd5a916163831c609d5
lwj <wj981031@163.com>
2020-08-19 16:08:12 +0800

    First commit

---
+++ demo01.py
@@ -0,0 +1,15 @@
+#-*- coding = utf-8 -*-
+#@Time : 2020/8/19 15:09
+#@Author : 李吴际
+#@File : demo01.py
+#@software : PyCharm
+'''
+print("hello world","cxk",sep=".",end="!")
+print(type("a"))
+
+
+'''
+a = input("输入：")
+print(type(a))
+print("一个数字 %d"%(int(a)))
+
